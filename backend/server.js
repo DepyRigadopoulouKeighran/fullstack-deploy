0fdf6f71976a49b3f7bb0db22b06fb2606354b70
Despoina Rigadopoulou-Keighran <lightblue@duck.com>
2024-04-09 11:05:00 +0200

    this is ready

--- backend/server.js
+++ backend/server.js
@@ -1,35 +1,37 @@
-import express from "express"
-import { config } from "dotenv"
-import connectDB from "./lib/database.js"
-import { authRouter } from "./src/routes/authRouter.js"
-import cors from "cors"
-import { serviceRouter } from "./src/routes/serviceRouter.js"
-const app = express()
-config()
-app.use(express.json())
-connectDB()
-app.use(cors())
-const port = process.env.PORT
+import express from "express";
+import { config } from "dotenv";
+import connectDB from "./lib/database.js";
+import { authRouter } from "./src/routes/authRouter.js";
+import cors from "cors";
+import { serviceRouter } from "./src/routes/serviceRouter.js";
+import path from "path";
+import { fileURLToPath } from "url";
+const app = express();
+config();
+app.use(express.json());
+connectDB();
+app.use(cors());
+const port = process.env.PORT;
 
+const currentFileLocation = fileURLToPath(import.meta.url);
 
-app.use("/auth", authRouter )
-app.use("/services",serviceRouter)
+const __dirname = path.dirname(currentFileLocation);
 
+app.use(express.static(path.join(__dirname, "/public/frontend/dist")));
 
+app.use("/auth", authRouter);
+app.use("/services", serviceRouter);
 
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/frontend/dist/index.html"));
+});
 
+app.use((err, req, res, next) => {
+  res
+    .status(err.status || 500)
+    .send({ error: err.message || "Something went wrong!" });
+});
 
-
-app.use((err,req,res,next)=>{
-
-res.status(err.status||500).send({error:err.message||  "Something went wrong!" })
-
-
-})
-
-
-app.listen(port,()=>{
-
-console.log(`[SERVER] running on http://localhost:${port}`)
-
-})
+app.listen(port, () => {
+  console.log(`[SERVER] running on http://localhost:${port}`);
+});
